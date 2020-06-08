8e30962a454a31298e0c3a833ddf4f28b7d7103a
Cheery <36590@qq.com>
2020-06-08 12:26:41 +0800

    GuoJia

--- index.js
+++ index.js
@@ -82,7 +82,7 @@ var KOCDatetime = {
     }
     if (dDiff < 0) {
       /* C */
-      var daysInLastFullMonth = Moment(ValueEnd.year() + '-' + (ValueEnd.month() + 1), "YYYY-MM").subtract(1, 'months').daysInMonth();
+      var daysInLastFullMonth = Moment(ValueEnd.year() + "-" + (ValueEnd.month() + 1), "YYYY-MM").subtract(1, "months").daysInMonth();
       if (daysInLastFullMonth < ValueBegin.date()) { // 31/01 -> 2/03
         dDiff = daysInLastFullMonth + dDiff + (ValueBegin.date() - daysInLastFullMonth);
       } else {
@@ -94,81 +94,160 @@ var KOCDatetime = {
       mDiff = 12 + mDiff;
       yDiff--;
     }
-    return {
-      Years: Moment.duration(yDiff, 'year').asYears(),
-      Months: Moment.duration(mDiff, 'month').asMonths(),
-      Days: Moment.duration(dDiff, 'day').asDays(),
-      Hours: Moment.duration(hourDiff, 'hour').asHours(),
-      Minutes: Moment.duration(minDiff, 'minute').asMinutes(),
-      Seconds: Moment.duration(secDiff, 'second').asSeconds(),
-      After: IsAfter
+    var retData = {
+      Years: Moment.duration(yDiff, "year").asYears(),
+      Months: Moment.duration(mDiff, "month").asMonths(),
+      Days: Moment.duration(dDiff, "day").asDays(),
+      Hours: Moment.duration(hourDiff, "hour").asHours(),
+      Minutes: Moment.duration(minDiff, "minute").asMinutes(),
+      Seconds: Moment.duration(secDiff, "second").asSeconds(),
+      SecondsDiff: { // 以秒计算时差
+        Total: parseInt(Moment.duration(ValueEnd.diff(ValueBegin)).asSeconds()), // 总计相差秒
+        Hours: 0,
+        Minutes: 0,
+        Seconds: 0
+      },
+      After: IsAfter,
+      Data: []
     };
+    retData.SecondsDiff.Seconds = retData.SecondsDiff.Total
+    retData.SecondsDiff.Hours = parseInt(retData.SecondsDiff.Total / 3600)
+    retData.SecondsDiff.Seconds -= parseInt(retData.SecondsDiff.Hours * 3600)
+    retData.SecondsDiff.Minutes = parseInt(retData.SecondsDiff.Seconds / 60)
+    retData.SecondsDiff.Seconds -= parseInt(retData.SecondsDiff.Minutes * 60)
+    var dataBegin = false
+    if (retData.Years) {
+      retData.Data.push({
+        Type: "Years",
+        Value: retData.Years,
+        Text: "年"
+      })
+      dataBegin = true
+    }
+    if (dataBegin || retData.Months) {
+      retData.Data.push({
+        Type: "Months",
+        Value: retData.Months,
+        Text: "月"
+      })
+      dataBegin = true
+    }
+    if (dataBegin || retData.Days) {
+      retData.Data.push({
+        Type: "Days",
+        Value: retData.Days,
+        Text: "天"
+      })
+      dataBegin = true
+    }
+    if (dataBegin || retData.Hours) {
+      retData.Data.push({
+        Type: "Hours",
+        Value: retData.Hours,
+        Text: "小时"
+      })
+      dataBegin = true
+    }
+    if (dataBegin || retData.Minutes) {
+      retData.Data.push({
+        Type: "Minutes",
+        Value: retData.Minutes,
+        Text: "分"
+      })
+      dataBegin = true
+    }
+    if (dataBegin || retData.Seconds) {
+      retData.Data.push({
+        Type: "Seconds",
+        Value: retData.Seconds,
+        Text: "秒"
+      })
+    }
+    return retData;
+  },
+  /********************************
+   * PreciseRangeText 取得时间差值文字(小时、分、秒)
+   ********************************/
+  // {
+  //   Begin: 开始时间,
+  //   End: 结束时间, // undefined:当前时间
+  //   Chinese: 是否中文描述 xx小时xx分xx秒 / xx:xx:xx
+  // }
+  PreciseRangeTimeText: function (val) {
+    var Value = KOCDatetime.PreciseRange(val.Begin, val.End);
+    if (!Value) {
+      return null;
+    }
+    return (Value.SecondsDiff.Hours >= 10 ? '' : '0') + Value.SecondsDiff.Hours + (val.Chinese ? '小时' : ':') + (Value.SecondsDiff.Minutes >= 10 ? '' : '0') + Value.SecondsDiff.Minutes + (val.Chinese ? '分' : ':') + (Value.SecondsDiff.Seconds >= 10 ? '' : '0') + Value.SecondsDiff.Seconds + (val.Chinese ? '秒' : '')
   },
   /********************************
    * PreciseRangeText 取得时间差值文字
    ********************************/
-  PreciseRangeText: function (ValueBegin, ValueEnd, Num) {
-    if (typeof ValueEnd === "number") {
-      Num = ValueEnd;
-      ValueEnd = null;
-    }
+  // {
+  //   Begin: 开始时间,
+  //   End: 结束时间, // undefined:当前时间
+  //   Num: 精度 // 从大到小,
+  //   Desc: 是否需要描述 // 之前 or 之后
+  // }
+  PreciseRangeText: function (val) {
     /* C */
-    var Value = KOCDatetime.PreciseRange(ValueBegin, ValueEnd);
+    var Value = KOCDatetime.PreciseRange(val.Begin, val.End);
     if (!Value) {
       return "";
     }
-    Num = KOCString.ToInt(Num, -1);
+    val.Num = KOCString.ToInt(val.Num, -1);
     /* L */
     var Text = "";
     /* L */
     var Space = false;
-    if (Num !== 0 && Value.Years) {
+    if (val.Num !== 0 && Value.Years) {
       Text += Value.Years + "年";
-      Num--;
+      val.Num--;
     }
-    if (Num !== 0) {
+    if (val.Num !== 0) {
       if (Value.Months) {
         Text += Value.Months + "个月";
-        Num--;
+        val.Num--;
       } else if (Text) {
         Space = true;
-        Num--;
+        val.Num--;
       }
     }
-    if (Num !== 0) {
+    if (val.Num !== 0) {
       if (Value.Days) {
         Text += (Space ? " 零 " : "") + Value.Days + "天";
         Space = false;
-        Num--;
+        val.Num--;
       } else if (Text) {
         Space = true;
-        Num--;
+        val.Num--;
       }
     }
-    if (Num !== 0) {
+    if (val.Num !== 0) {
       if (Value.Hours) {
         Text += (Space ? " 零 " : "") + Value.Hours + "小时";
         Space = false;
-        Num--;
+        val.Num--;
       } else if (Text) {
         Space = true;
-        Num--;
+        val.Num--;
       }
     }
-    if (Num !== 0) {
+    if (val.Num !== 0) {
       if (Value.Minutes) {
         Text += (Space ? " 零 " : "") + Value.Minutes + "分";
         Space = false;
-        Num--;
+        val.Num--;
       } else if (Text) {
         Space = true;
-        Num--;
+        val.Num--;
       }
     }
-    if (Num !== 0 && Value.Seconds) {
+    if (val.Num !== 0 && Value.Seconds) {
       Text += (Space ? " 零 " : "") + Value.Seconds + "秒";
     }
-    return Text ? (Text + " " + (Value.After ? "以前" : "以后")) : "刚刚";
+    val.Desc && (Text ? (Text + " " + (Value.After ? "以前" : "以后")) : "刚刚");
+    return Text;
   },
   /********************************
    * Min 取得最小时间(Moment)
@@ -307,67 +386,67 @@ var KOCDatetime = {
     };
     switch (type) {
       default:
-      case 'Day-N':
+      case "Day-N":
         _Date.Begin = Moment();
         _Date.End = Moment();
         break;
-      case 'Day-1':
-        _Date.Begin = Moment().add(-1, 'day');
-        _Date.End = Moment().add(-1, 'day');
+      case "Day-1":
+        _Date.Begin = Moment().add(-1, "day");
+        _Date.End = Moment().add(-1, "day");
         break;
-      case 'Day-2':
-        _Date.Begin = Moment().add(-2, 'day');
-        _Date.End = Moment().add(-2, 'day');
+      case "Day-2":
+        _Date.Begin = Moment().add(-2, "day");
+        _Date.End = Moment().add(-2, "day");
         break;
-      case 'Week-N':
-        _Date.Begin = Moment().startOf('isoWeek');
-        _Date.End = Moment().endOf('isoWeek');
+      case "Week-N":
+        _Date.Begin = Moment().startOf("isoWeek");
+        _Date.End = Moment().endOf("isoWeek");
         break;
-      case 'Week-1':
-        _Date.Begin = Moment().add(-1, 'week').startOf('isoWeek');
-        _Date.End = Moment().add(-1, 'week').endOf('isoWeek');
+      case "Week-1":
+        _Date.Begin = Moment().add(-1, "week").startOf("isoWeek");
+        _Date.End = Moment().add(-1, "week").endOf("isoWeek");
         break;
-      case 'Week-1-N':
-        _Date.Begin = Moment().add(-1, 'week').startOf('isoWeek');
-        _Date.End = Moment().endOf('isoWeek');
+      case "Week-1-N":
+        _Date.Begin = Moment().add(-1, "week").startOf("isoWeek");
+        _Date.End = Moment().endOf("isoWeek");
         break;
-      case 'Month-N':
-        _Date.Begin = Moment().startOf('month');
-        _Date.End = Moment().endOf('month');
+      case "Month-N":
+        _Date.Begin = Moment().startOf("month");
+        _Date.End = Moment().endOf("month");
         break;
-      case 'Month-1':
-        _Date.Begin = Moment().add(-1, 'month').startOf('month');
-        _Date.End = Moment().add(-1, 'month').endOf('month');
+      case "Month-1":
+        _Date.Begin = Moment().add(-1, "month").startOf("month");
+        _Date.End = Moment().add(-1, "month").endOf("month");
         break;
-      case 'Month-1-N':
-        _Date.Begin = Moment().add(-1, 'month').startOf('month');
-        _Date.End = Moment().endOf('month');
+      case "Month-1-N":
+        _Date.Begin = Moment().add(-1, "month").startOf("month");
+        _Date.End = Moment().endOf("month");
         break;
-      case 'Quarter-N':
-        _Date.Begin = Moment().startOf('quarter');
-        _Date.End = Moment().endOf('quarter');
+      case "Quarter-N":
+        _Date.Begin = Moment().startOf("quarter");
+        _Date.End = Moment().endOf("quarter");
         break;
-      case 'Quarter-1':
-        _Date.Begin = Moment().add(-1, 'quarter').startOf('quarter');
-        _Date.End = Moment().add(-1, 'quarter').endOf('quarter');
+      case "Quarter-1":
+        _Date.Begin = Moment().add(-1, "quarter").startOf("quarter");
+        _Date.End = Moment().add(-1, "quarter").endOf("quarter");
         break;
-      case 'Year-N':
-        _Date.Begin = Moment().startOf('year');
-        _Date.End = Moment().endOf('year');
+      case "Year-N":
+        _Date.Begin = Moment().startOf("year");
+        _Date.End = Moment().endOf("year");
         break;
-      case 'Year-1':
-        _Date.Begin = Moment().add(-1, 'year').startOf('year');
-        _Date.End = Moment().add(-1, 'year').endOf('year');
+      case "Year-1":
+        _Date.Begin = Moment().add(-1, "year").startOf("year");
+        _Date.End = Moment().add(-1, "year").endOf("year");
         break;
-      case 'Year-2':
-        _Date.Begin = Moment().add(-2, 'year').startOf('year');
-        _Date.End = Moment().add(-2, 'year').endOf('year');
+      case "Year-2":
+        _Date.Begin = Moment().add(-2, "year").startOf("year");
+        _Date.End = Moment().add(-2, "year").endOf("year");
         break;
     }
     _Date.Begin = KOCDatetime.Min(_Date.Begin, false);
-    _Date.BeginString = _Date.Begin.format('YYYY-MM-DD HH:mm:ss');
+    _Date.BeginString = _Date.Begin.format("YYYY-MM-DD HH:mm:ss");
     _Date.End = KOCDatetime.Min(_Date.End, false);
-    _Date.EndString = _Date.End.format('YYYY-MM-DD HH:mm:ss');
+    _Date.EndString = _Date.End.format("YYYY-MM-DD HH:mm:ss");
     return _Date;
   }
 };
